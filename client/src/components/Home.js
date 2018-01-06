48ba566270dac3f283b17df7c82d775f0c5e99a7
Taj Calles <tajcalles@gmail.com>
2018-01-06 14:34:38 -0700

    create menu tabs

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
-import { Container, Header } from 'semantic-ui-react';
+import { Container, Header, Tab } from 'semantic-ui-react';
+
+const panes = [
+  { menuItem: 'lunchdinner', render: () => <Tab.Pane attached={false}>Lunch & Dinner</Tab.Pane> },
+  { menuItem: 'beverages', render: () => <Tab.Pane attached={false}>Breverages</Tab.Pane> },
+  { menuItem: 'deserts', render: () => <Tab.Pane attached={false}>Deserts</Tab.Pane> },
+]
 
 class Home extends Component {
   render() {
@@ -7,6 +13,7 @@ class Home extends Component {
       <Container>
         <Header as='h1' textAlign='center'>Charlie Chow's</Header>
         <Header as='h2' textAlign='center'>Dragon Grill</Header>
+        <Tab menu={{ pointing: true }} panes={panes} />
       </Container>
     );
   }
