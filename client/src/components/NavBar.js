6b0e5de13e2324ae98eb4530af889d7845f37686
Jace P. Gold <Jace@JaceGold.com>
2018-01-06 13:29:15 -0700

    Invered nav

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -34,22 +34,34 @@ class NavBar extends Component {
   render() {
     const { activeItem } = this.state
     return (
-      <div>
-        <Menu pointing secondary>
+      <nav>
+        <Menu pointing inverted style={ styles.main_menu }>
           <Link to='/'>
             <Menu.Item name='Home' active={activeItem === 'home'} />
-          </Link>
-          <Link to='/about'>
-            <Menu.Item name='Our History' active={activeItem === 'about'} />
           </Link>
           <Link to='/menu'>
             <Menu.Item name='Menu' active={activeItem === 'menu'} />
           </Link>
+          <Link to='/menu'>
+            <Menu.Item name='Catering' active={activeItem === 'catering'} />
+          </Link>
+          <Link to='/menu'>
+            <Menu.Item name='Contact' active={activeItem === 'contact'} />
+          </Link>
+          <Link to='/about'>
+            <Menu.Item name='Our History' active={activeItem === 'about'} />
+          </Link>
           { this.rightNavs() }
         </Menu>
-      </div>
+      </nav>
     );
   }
+}
+
+const styles = {
+  main_menu: {
+    borderRadius: '0',
+  },
 }
 
 const mapStateToProps = state => {
