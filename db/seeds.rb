d1ada1721c98d624a0b0c2fbe78443c9bd9a1eea
Taj Calles <tajcalles@gmail.com>
2018-01-06 16:45:43 -0700

    added menu items

--- db/seeds.rb
+++ db/seeds.rb
@@ -22,5 +22,73 @@ menus.each_with_index do |menu, i|
     item_type: "Lunch & Dinner"
   )
 
+  MenuItem.create(
+    name: "Pepper Shrimp/Scallops",
+    price: 12.50,
+    description: "Peppery onions and peppers stir-fired in an oyster sauce with ground peppercorns.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+
+  MenuItem.create(
+    name: "Singapore Honey Shrimp",
+    price: 14.50,
+    description: "Butterflied shrimp in a light ginger batter, and served with a sweet lemon sauce.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+
+  MenuItem.create(
+    name: "Kung Pao Shrimp/Scallops:",
+    price: 12.95,
+    description: "Spicy stir-fried dish with mixed vegetables, topped with whole roasted peanuts",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+
+  MenuItem.create(
+    name: "Honey Glazed Walnut Shrimp",
+    price: 14.95,
+    description: "Our Chef's special dish. Shrimp Prepared in an exquisite sauce worthy of an emperor.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+  MenuItem.create(
+    name: "Coconut Shrimp",
+    price: 12.95,
+    description: "Butterflied shrimp in a coconut batter, deep fried, and served with a hot and sweet Thai dipping sauce.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+
+  MenuItem.create(
+    name: "Cashew Chicken",
+    price: 10.55,
+    description: "The Hong Kong tradition-roasted Cashews with assorted Chinese vegetables and fresh mushrooms.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+
+  MenuItem.create(
+    name: "Lemon Chicken",
+    price: 10.55,
+    description: "Battered chicken breast, deep fried, served with our lemon sauce on a bed of sweet cabbage.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+  MenuItem.create(
+    name: "Spicy Stir-Fried Orange Peel Beef",
+    price: 10.55,
+    description: "Braised chicken breast, stir-fried in orange zest, whole chilies, red ginger, served on a bed of nappa cabbage.",
+    spicy: false,
+    item_type: "Lunch & Dinner"
+  )
+  MenuItem.create(
+    name: "Coca-Cola",
+    price: 2.95,
+    description: "Soda-Type Beverage",
+    spicy: false,
+    item_type: "Beverages"
+  )
 
 end
