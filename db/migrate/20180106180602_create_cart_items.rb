498db419a5a4736158665f63dfd9790dccac2689
Zach Byers <byerszach@gmail.com>
2018-01-06 13:24:36 -0700

    controllers functioning

--- db/migrate/20180106180602_create_cart_items.rb
+++ db/migrate/20180106180602_create_cart_items.rb
@@ -1,7 +1,7 @@
 class CreateCartItems < ActiveRecord::Migration[5.1]
   def change
     create_table :cart_items do |t|
-      t.jsonb :substitutions
+      t.jsonb :special_instructions
       t.belongs_to :menu_item, foreign_key: true
       t.belongs_to :order, foreign_key: true
 
