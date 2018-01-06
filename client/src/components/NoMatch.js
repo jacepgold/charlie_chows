391c75d7e4fc8e01b0fc09673c80848c039ba928
Pete Alberico <palberico@yahoo.com>
2018-01-06 14:13:04 -0700

    404 page

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
-import { Header } from 'semantic-ui-react';
+import { Header, Container, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
+import Logo from '../images/ChowsLogo.png';
 
 class NoMatch extends Component {
   render() {
     return (
-      <Header as='h1' textAlign='center'>
-        Page Not Found
-        <Link to='/'> Home</Link>
-      </Header>
+      <Container text>
+      <Header>
+      
+       <h2>404 Error Page -- "No you go wrong way...you go <Link to='/'>back</Link> now!"</h2>
+        <br />
+        <br />
+        <div align='center'>
+          <iframe src="https://giphy.com/embed/46yeAcewRq7Re" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/46yeAcewRq7Re"></a></p>
+       </div>
+       <br />
+       <div align='center'>
+          <img style={styles.logo} src={Logo} alt='Charlie Chows Dragon Grill' />
+        </div>
+     
+        </Header>
+     </Container>
     );
+  }
+}
+
+const styles = {
+  logo: {
+      width: '500px'
   }
 }
 
