a3edb09475aeebf7a658cbf7db05ea60ad71fe23
Zach Byers <byerszach@gmail.com>
2018-01-06 11:43:47 -0700

    relations work

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.create(
+  name: "Charlie Chow's",
+  address: '255 East 400 South SLC UT',
+  description: "Charlie Chow's Dragon Grill has always been known for its fine Asian cuisine served in an attractive, contemporary setting in the heart of downtown Salt Lake City. Be sure to sample our fresh, homemade Dim Sum (appetizer) in addition to the delicious entrees made to order and elegantly served. And don't forget to try the Dragon Grill, a tasty new way to	create your own meal and sample a varitey of Asian flavors. Because all of our entrees are made fresh to order, we use no MSG in our cooking. Plus, our traditon of friendly service and great food have made Charlie Chow's Dragon Grill a long time Salt Lake favorite."
+)
+
+menus = ['Appetizers', 'Entrees', 'Beverages', 'Desserts']
+
+menus.each do |menu, i|
+  menu = Menu.create(menu_type: menu, restaurant_id: 1)
+  order = Order.create(
+    customer_address: Faker::Address.street_address,
+    customer_first_name: Faker::Name.first_name,
+    customer_last_name: Faker::Name.last_name,
+    customer_phone: Faker::PhoneNumber.phone_number,
+    restaurant_id: 1
+  )
+
+  5.times do
+    item = menu.menu_items.create(
+      name: Faker::Food.dish,
+      price: 3.5,
+      description: Faker::Lorem.sentence,
+      spicy: false,
+      menu_id: menu.id
+    )
+    item.cart_items.create(order_id: order.id)
+  end
+
+
+end
