a90e3fbd5ded85492027ee6f3e87f5ab832e2611
Pete Alberico <palberico@yahoo.com>
2018-01-06 15:06:27 -0700

    Our History

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { Container, Header } from 'semantic-ui-react';
+import Chow from '../images/CharlieChowPortrate.png';
 
 class About extends Component {
   render() {
     return (
       <div>
         <Container>
-          <Header as='h1' textAlign='center'>Our History</Header>
-          <p>
+        <div align='center'>
+          <img style={styles.logo} src={Chow} alt='Charlie Chows Dragon Grill' />
+        </div>
+          <Header as='h1' style={styles.history} >OUR HISTORY</Header>
+          <h2>
             Charlie Chow's Dragon Grill has always been known for it's fine
             Asian cuisine served in an attractive, contemporary setting in the 
             heart of downtown Salt Lake City. Be sure to sample our fresh, homemade
@@ -17,11 +21,22 @@ class About extends Component {
             entrees are made fresh to order, we use no MSG in our cooking. Plus, our traditon of 
             friendly service and great food have made Charlie Chow's Dragon Grill a long time 
             Salt Lake favorite.
-          </p>
+          </h2>
         </Container>
       </div>
     );
   }
+}
+
+const styles = {
+  logo: {
+      width: '200px',
+      borderRadius: 300,
+  },
+  history: {
+    textAlign: 'center',
+    textDecoration: 'underline',
+}
 }
 
 export default About;
--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Header, Container, Segment } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
-import Logo from '../images/ChowsLogo.png';
+import Logo from '../images/ChowsLogoWhite.png';
 
 class NoMatch extends Component {
   render() {
@@ -11,14 +11,10 @@ class NoMatch extends Component {
       
        <h2>404 Error Page -- "No you go wrong way...you go <Link to='/'>back</Link> now!"</h2>
         <br />
-        <br />
         <div align='center'>
           <iframe src="https://giphy.com/embed/46yeAcewRq7Re" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/46yeAcewRq7Re"></a></p>
        </div>
        <br />
-       <div align='center'>
-          <img style={styles.logo} src={Logo} alt='Charlie Chows Dragon Grill' />
-        </div>
      
         </Header>
      </Container>
